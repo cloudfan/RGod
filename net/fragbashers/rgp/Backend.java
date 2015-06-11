7798d980d0dab07a086cfa9d760c8dc64081a0f4
cloudfan <deadzyeti@gmail.com>
2015-06-11 01:29:36 -0500

    File Download update
    Significant update to how RGP downloads god lists and how frequently (now once per application startup, as it should have been)

--- net/fragbashers/rgp/Backend.java
+++ net/fragbashers/rgp/Backend.java
@@ -1,5 +1,6 @@
 package net.fragbashers.rgp;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -7,62 +8,70 @@ import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
+import org.apache.commons.io.FileUtils;
+
 public class Backend {
 	
-	public static String getAll() {
+	public static void DownloadLists(){
+		    try {
+		    	
+		    	URL as = new URL("http://www.fragbashers.net/smiteapp/assassin.txt");
+		    	URL gd = new URL("http://www.fragbashers.net/smiteapp/guardian.txt");
+		    	URL hn = new URL("http://www.fragbashers.net/smiteapp/hunter.txt");
+		    	URL mg = new URL("http://www.fragbashers.net/smiteapp/mage.txt");
+		    	URL wr = new URL("http://www.fragbashers.net/smiteapp/warrior.txt");
+		    	
+		    	FileUtils.copyURLToFile(as, new File("C:/temp/assassin.txt"));
+		    	FileUtils.copyURLToFile(gd, new File("C:/temp/guardian.txt"));
+		    	FileUtils.copyURLToFile(hn, new File("C:/temp/hunter.txt"));
+		    	FileUtils.copyURLToFile(mg, new File("C:/temp/mage.txt"));
+		    	FileUtils.copyURLToFile(wr, new File("C:/temp/warrior.txt"));
+		
+		    } catch (Exception e) {
+			
+		    }
+		
+	}
+	
+	public static String getAll() throws Exception {
 		List<String> gtor = new ArrayList<String>();
 		Random rand = new Random();
-		
-		try {
-			URL as = new URL("http://www.fragbashers.net/smiteapp/assassin.txt");
-			URL gd = new URL("http://www.fragbashers.net/smiteapp/guardian.txt");
-			URL hn = new URL("http://www.fragbashers.net/smiteapp/hunter.txt");
-			URL mg = new URL("http://www.fragbashers.net/smiteapp/mage.txt");
-			URL wr = new URL("http://www.fragbashers.net/smiteapp/warrior.txt");
-			Scanner sas = new Scanner(as.openStream());
-			Scanner sgd = new Scanner(gd.openStream());
-			Scanner shn = new Scanner(hn.openStream());
-			Scanner smg = new Scanner(mg.openStream());
-			Scanner swr = new Scanner(wr.openStream());
+	
+		Scanner sas = new Scanner(new File ("C:/temp/assassin.txt"));
+		Scanner sgd = new Scanner(new File ("C:/temp/guardian.txt"));
+		Scanner shn = new Scanner(new File ("C:/temp/hunter.txt"));
+		Scanner smg = new Scanner(new File ("C:/temp/mage.txt"));
+		Scanner swr = new Scanner(new File ("C:/temp/warrior.txt"));
 
-			while (sas.hasNext())
-			{
-			    String str = sas.nextLine();
-			    gtor.add(str);
-			}
-			while (sgd.hasNext())
-			{
-			    String str = sgd.nextLine();
-			    gtor.add(str);
-			}
-			while (shn.hasNext())
-			{
-			    String str = shn.nextLine();
-			    gtor.add(str);
-			}
-			while (smg.hasNext())
-			{
-			    String str = smg.nextLine();
-			    gtor.add(str);
-			}
-			while (swr.hasNext())
-			{
-			    String str = swr.nextLine();
-			    gtor.add(str);
-			}
-			//System.out.println(gtor);
-			sas.close();
-			sgd.close();
-			shn.close();
-			smg.close();
-			swr.close();
-		} catch(IOException ex) {
-			ex.printStackTrace();
+		while (sas.hasNext()) {
+		    String str = sas.nextLine();
+		    gtor.add(str);
 		}
+		while (sgd.hasNext()) {
+		    String str = sgd.nextLine();
+		    gtor.add(str);
+		}
+		while (shn.hasNext()) {
+		    String str = shn.nextLine();
+		    gtor.add(str);
+		}
+		while (smg.hasNext()) {
+		    String str = smg.nextLine();
+		    gtor.add(str);
+		}
+		while (swr.hasNext()) {
+		    String str = swr.nextLine();
+		    gtor.add(str);
+		}
+		sas.close();
+		sgd.close();
+		shn.close();
+		smg.close();
+		swr.close();
 		
 		int index = rand.nextInt(gtor.size());
 		String rangod = gtor.get(index);
-		System.out.println("Random God = " + rangod);
+		//System.out.println("Random God = " + rangod);
 		return rangod;
 	}
 
@@ -70,14 +79,11 @@ public class Backend {
 		List<String> las = new ArrayList<String>();
 		
 		try {
-			URL as = new URL("http://www.fragbashers.net/smiteapp/assassin.txt");
-			Scanner sas = new Scanner(as.openStream());
-			while (sas.hasNext())
-			{
+			Scanner sas = new Scanner(new File ("C:/temp/assassin.txt"));
+			while (sas.hasNext()) {
 			    String str = sas.nextLine();
 			    las.add(str);
 			}
-			System.out.println(las);
 			sas.close();
 		} catch(IOException ex) {
 			ex.printStackTrace();
@@ -89,14 +95,11 @@ public class Backend {
 		List<String> lgd = new ArrayList<String>();
 		
 		try {
-			URL gd = new URL("http://www.fragbashers.net/smiteapp/guardian.txt");
-			Scanner sgd = new Scanner(gd.openStream());
-			while (sgd.hasNext())
-			{
+			Scanner sgd = new Scanner(new File ("C:/temp/guardian.txt"));
+			while (sgd.hasNext()) {
 			    String str = sgd.nextLine();
 			   	lgd.add(str);
 			}
-			System.out.println(lgd);
 			sgd.close();
 		} catch(IOException ex) {
 			ex.printStackTrace();
@@ -108,14 +111,11 @@ public class Backend {
 		List<String> lhn = new ArrayList<String>();
 		
 		try {
-			URL hn = new URL("http://www.fragbashers.net/smiteapp/hunter.txt");
-			Scanner shn = new Scanner(hn.openStream());
-			while (shn.hasNext())
-			{
+			Scanner shn = new Scanner(new File ("C:/temp/hunter.txt"));
+			while (shn.hasNext()) {
 			    String str = shn.nextLine();
 			    lhn.add(str);
 			}
-			System.out.println(lhn);
 			shn.close();
 		} catch(IOException ex) {
 			ex.printStackTrace();
@@ -127,14 +127,11 @@ public class Backend {
 		List<String> lmg = new ArrayList<String>();
 		
 		try {
-			URL mg = new URL("http://www.fragbashers.net/smiteapp/mage.txt");
-			Scanner smg = new Scanner(mg.openStream());
-			while (smg.hasNext())
-			{
+			Scanner smg = new Scanner(new File ("C:/temp/mage.txt"));
+			while (smg.hasNext()) {
 			    String str = smg.nextLine();
 			    lmg.add(str);
 			}
-			System.out.println(lmg);
 			smg.close();
 		} catch(IOException ex) {
 			ex.printStackTrace();
@@ -146,18 +143,30 @@ public class Backend {
 		List<String> lwr = new ArrayList<String>();
 		
 		try {
-			URL wr = new URL("http://www.fragbashers.net/smiteapp/warrior.txt");
-			Scanner swr = new Scanner(wr.openStream());
-			while (swr.hasNext())
-			{
+			Scanner swr = new Scanner(new File ("C:/temp/warrior.txt"));
+			while (swr.hasNext()) {
 			    String str = swr.nextLine();
 			    lwr.add(str);
 			}
-			System.out.println(lwr);
 			swr.close();
 		} catch(IOException ex) {
 			ex.printStackTrace();
 		}
 		return lwr;
+	}
+
+	public static String getRole() {
+		List<String> rol = new ArrayList<String>();
+		rol.add("ADC");
+		rol.add("Support");
+		rol.add("Mid");
+		rol.add("Solo");
+		rol.add("Jungle");
+		
+		Random rand = new Random();
+		int rindex = rand.nextInt(rol.size());
+		String role = rol.get(rindex);
+		
+		return role;
 	}
 }

935cdf843881999d513c5a8d8bc7468a91b797d2
cloudfan <deadzyeti@gmail.com>
2015-06-22 04:25:58 -0500

    2.999.999 Update
    Changes to add the ability to play audio

--- net/fragbashers/rgp/Interface.java
+++ net/fragbashers/rgp/Interface.java
@@ -1,27 +1,6 @@
 package net.fragbashers.rgp;
 
 import java.awt.Color;
-import java.awt.EventQueue;
-import java.awt.Font;
-import java.awt.Point;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
-import javax.swing.JCheckBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.SwingConstants;
-import javax.swing.UIManager;
-import javax.swing.border.LineBorder;
-
-import com.jgoodies.forms.factories.DefaultComponentFactory;
 
 /**
  * @author Liam
@@ -42,12 +21,13 @@ public class Interface extends JFrame{
 	private boolean getMage = false;
 	private boolean getWarrior = false;
 	private boolean chooseRole = false;
+	private boolean audio = true;
+	HoverButton btnMute = new HoverButton("");
 	
 	public Interface() {
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (Exception e ){
-			
+		} catch (Exception e){
 		}
 		
 		String rgod = "";
@@ -61,7 +41,8 @@ public class Interface extends JFrame{
 		final JCheckBox chckbxWarrior = new JCheckBox("Warrior");
 		final JCheckBox chckbxRandomRole = new JCheckBox("Random Role");
 
-		final JLabel lblCopyright = new JLabel("v2.0 by Fragbashers ©");
+		final JLabel lblCopyright = new JLabel("Fragbashers ©");
+		lblCopyright.setForeground(Color.LIGHT_GRAY);
 		final JLabel lblGodName = DefaultComponentFactory.getInstance().createLabel(rgod);
 		final JLabel lblRandom = DefaultComponentFactory.getInstance().createLabel("Your Random God is:");
 		final JLabel lblRole = DefaultComponentFactory.getInstance().createLabel(rrole);
@@ -108,7 +89,7 @@ public class Interface extends JFrame{
 		HoverButton btnNewGod = new HoverButton("New God");
 		btnNewGod.setForeground(new Color(50, 205, 50));
 		btnNewGod.setFont(new Font("Estrangelo Edessa", Font.PLAIN, 14));
-		btnNewGod.setBounds(16, 139, 60, 14);
+		btnNewGod.setBounds(16, 142, 60, 14);
 		cont.add(btnNewGod);
 		btnNewGod.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -121,14 +102,31 @@ public class Interface extends JFrame{
 				
 				try {
 				if (getAssassin & getGuardian & getHunter & getMage & getWarrior) {
-					//System.out.println("Getting All.");
 					String rangod = Backend.getAll();
 					lblGodName.setText(rangod);
+					if (audio) {
+						try {
+							AudioInputStream stream;
+							AudioFormat format;
+							DataLine.Info info;
+							Clip clip;
+							
+							stream = AudioSystem.getAudioInputStream(new File("res/"+rangod+".wav"));
+							format = stream.getFormat();
+							info = new DataLine.Info(Clip.class, format);
+							clip = (Clip) AudioSystem.getLine(info);
+							clip.open(stream);
+							FloatControl gainControl = 
+									(FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+							gainControl.setValue(-10.0f);
+							clip.start();
+						}
+						catch (Exception e1) {
+						}
+					}
 				} else {
 					List<String> someRoles = new ArrayList<String>();
 					Random rand = new Random();
-					//System.out.println("Getting Most.");
-					
 					if (getAssassin) {
 			            someRoles.addAll(Backend.getAs());
 			        }
@@ -147,15 +145,32 @@ public class Interface extends JFrame{
 					int index = rand.nextInt(someRoles.size());
 					String rangod = someRoles.get(index);
 					lblGodName.setText(rangod);
-					//System.out.println("Random God = " + rangod);
+					if (audio) {
+						try {
+					    	AudioInputStream stream;
+					    	AudioFormat format;
+					    	DataLine.Info info;
+					    	Clip clip;
+
+					    	stream = AudioSystem.getAudioInputStream(new File("res/"+rangod+".wav"));
+					    	format = stream.getFormat();
+					    	info = new DataLine.Info(Clip.class, format);
+					    	clip = (Clip) AudioSystem.getLine(info);
+					    	clip.open(stream);
+					    	FloatControl gainControl = 
+					    		(FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+					    	gainControl.setValue(-10.0f);
+					    	clip.start();
+						}
+						catch (Exception e1) {
+						}
+					}
 				}
 				if (chooseRole) {
 					String ranRole = Backend.getRole();
 					lblRole.setText(ranRole);
-					//System.out.println("Random Role = " + ranRole);
 				}	
 				} catch (Exception ex){
-					//System.out.println("No gods chosen!");
 					lblGodName.setText("No gods chosen!");
 				}
 			}
@@ -246,6 +261,23 @@ public class Interface extends JFrame{
 		btnExit.setBounds(275, 10, 15, 15);
 		cont.add(btnExit);
 		
+		
+		btnMute.setText("\u266B");
+		btnMute.setFont(new Font("Arial", Font.PLAIN, 15));
+		btnMute.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				audio = !audio;
+				if (audio) {
+					btnMute.setForeground(Color.GREEN);
+				} else {
+					btnMute.setForeground(Color.RED);
+				}
+			}
+		});
+		btnMute.setForeground(Color.GREEN);
+		btnMute.setBounds(258, 9, 17, 17);
+		cont.add(btnMute);
+		
 		setVisible(true);
 	}
 	
@@ -253,9 +285,13 @@ public class Interface extends JFrame{
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Interface frame = new Interface();
-					Backend.DownloadLists();
-					frame.setVisible(true);
+					if (!Backend.CheckUpdate()) {
+						Interface frame = new Interface();
+						Backend.DownloadLists();
+						frame.setVisible(true);
+					} else {
+						new Updater();
+					}
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
